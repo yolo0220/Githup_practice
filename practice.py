485bd0c9baf72a615b13234375f4b24d263ebc91
Juhyeong Kim <champfp7@gmail.com>
2017-06-29 13:01:31 +0900

    g

---
+++ practice.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print("Hello World!")
+
+print "되나요" 
+# In[3]:
+
+print(" !@#$%^&*")
+
+
+# In[ ]:
+
+# 함께해보아요
+
+
+# In[1]:
+
+# 신나는 머신러닝
+
+
+# In[3]:
+
+print("Good Job")
+
+
+# In[2]:
+
+print("Ahaha")
+
+
+# In[1]:
+
+print("잘되네요")
+
+
+# In[ ]:
+
+
+
